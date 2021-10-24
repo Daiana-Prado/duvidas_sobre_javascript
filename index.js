5d4150c15f8d790ab848862411221ec2e43d2d7f
Daiana <daiana.prado8@gmail.com>
2021-10-24 20:10:16 -0300

    Aula javascript da Dio

---
+++ index.js
@@ -0,0 +1,17 @@
+var //indica uma variavel, giarda um balor na memoria.
+//int
+var meuInt = 1;
+//float
+var meuFloat = -2.3;
+//string
+var minhaString = "1212121"
+//boolean
+var meuBoolean1 = true; //1
+var meuBoolean2 = false; //0
+
+var pessoa = {
+    caoCabelo: "ruivo",//objeto guarda uma informação de chave valor.
+    idade: 24,
+};
+
+var meuArray = ['Maria', 1, -2.3, []]//array guarda instruturas em forma de lista
