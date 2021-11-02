860b9930e5a9f996ba85a5fe0f6333bec68589a4
Daiana <daiana.prado8@gmail.com>
2021-11-02 19:55:30 -0300

    dados

---
+++ Estrutura_de_Dados/strings.js
@@ -0,0 +1,13 @@
+//Estrutura de dados: No javascript são e tipagem dinamica e fraca, ou seja antes de declarar um valor vc não precisa especificar o tipo dele;
+//tipos de dados em js são distribuidos em dois grandes grupos;
+//primitivos: numbers,strings, booleans,null e indefinide
+//Compostos: objetcts e arrays
+const DIO = "Digitsl Innovation One";
+
+let firtName = "João";
+let lastName = "Luiz";
+
+let fullName = 'NomeCompleto: ${firtName} ${lastName}';
+
+//Strings são comunmente usadas para textos
+
