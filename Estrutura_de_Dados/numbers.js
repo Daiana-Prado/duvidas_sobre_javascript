4ba366e63e2b3b6b239fa603c2075a34a61e666e
Daiana <daiana.prado8@gmail.com>
2021-11-03 20:37:40 -0300

    Numeros

---
+++ Estrutura_de_Dados/numbers.js
@@ -0,0 +1,17 @@
+//Numeros podem ser inteiros ou decimais
+
+
+//boolean: são comparaçoes entre dois valores true e false
+//Exemplos:
+let validation = 3 === 0
+undefined
+validation
+false
+validation = 3 === 3
+true
+validation = 3 >= 3
+true
+validation = 3 <= 5
+true
+!validation
+false
